7ce1a8ad36ce94814e0fa6e917db4495a3e4cc07
Vasu <vasudhiman105@gmail.com>
2025-07-30 00:47:40 +0530

    testing deployment fixes

--- backend/lib/passport.js
+++ backend/lib/passport.js
@@ -1,46 +1,93 @@
-import passport from "passport";
+// import passport from "passport";
+// import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+// import User from "../models/userModel.js";
+// import dotenv from 'dotenv' 
+
+// dotenv.config();
+
+// passport.serializeUser(( user , done ) => {
+//     done(null, user.id)
+// })
+
+// passport.deserializeUser( async (id , done) => {
+//     try {
+//         const user = await User.findById(id);
+//         done(null , user)
+//     } catch (error) {
+//         done(err)
+//     }
+// })
+
+// passport.use(new GoogleStrategy({
+//     clientID: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     callbackURL: "/auth/google/callback"
+// },
+// async ( accessToken , refreshToken , profile , done) => {
+//     try {
+//         let user = await User.findOne({ googleId: profile.id });
+
+//         if (user) {
+//             return done(null , user)
+//         } else{
+//             const newUser = new User({
+//                 googleId : profile.id,
+//                 fullName: profile.displayName,
+//                 email: profile.emails[0].value
+//             });
+//             await newUser.save();
+//             return done(null, newUser)
+//         }
+
+//     } catch (error) {
+//         return done(error)
+//     }
+// }
+// ))
+
+
+import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import User from "../models/userModel.js";
-import dotenv from 'dotenv' 
+import dotenv from 'dotenv';
+import User from '../models/userModel.js';
 
 dotenv.config();
 
-passport.serializeUser(( user , done ) => {
-    done(null, user.id)
-})
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
 
-passport.deserializeUser( async (id , done) => {
+passport.deserializeUser(async (id, done) => {
     try {
         const user = await User.findById(id);
-        done(null , user)
-    } catch (error) {
-        done(err)
+        done(null, user);
+    } catch (err) {
+        done(err);
     }
-})
+});
 
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`
 },
-async ( accessToken , refreshToken , profile , done) => {
+async (accessToken, refreshToken, profile, done) => {
     try {
         let user = await User.findOne({ googleId: profile.id });
 
         if (user) {
-            return done(null , user)
-        } else{
+            return done(null, user);
+        } else {
             const newUser = new User({
-                googleId : profile.id,
+                googleId: profile.id,
                 fullName: profile.displayName,
-                email: profile.emails[0].value
+                email: profile.emails[0].value,
+                profilePicture: profile.photos[0].value
             });
             await newUser.save();
-            return done(null, newUser)
+            return done(null, newUser);
         }
-
-    } catch (error) {
-        return done(error)
+    } catch (err) {
+        return done(err);
     }
-}
-))
+}));
