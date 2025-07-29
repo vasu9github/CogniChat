e395e434483e2f1cb24a66475fe934e4a92df882
Vasu <vasudhiman105@gmail.com>
2025-07-30 00:19:57 +0530

    Changed Deployed URL

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://cogni-chat-six.vercel.app/', 
   credentials: true,
 }));
 app.use(express.json());
