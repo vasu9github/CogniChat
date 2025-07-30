c0c833e45bc4477a21bc4a015587e9302085103f
Vasu <vasudhiman105@gmail.com>
2025-07-30 09:24:18 +0530

    imported useEffect

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -244,6 +244,7 @@ import { Search, Plus, User, MessageSquare, Trash2, X } from 'lucide-react';
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Sidebar = ({ isOpen, toggleSidebar, setSelectedChatId, startNewChat, chats, setChats }) => {
   const { user } = useAuth();
